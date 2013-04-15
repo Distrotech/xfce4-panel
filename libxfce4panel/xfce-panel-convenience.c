ae4447d399aa111ba53ef9d317f7c1cf2709352c
Andrzej <ndrwrdck@gmail.com>
2013-04-15 19:45:17 +0100

    Revert "Workaround for PanelImage and GtkButton conflict (image size growing on click/hover)"
    This reverts commit a6b01d8b5dda51c8eedae4aa2c35627037719117.

--- libxfce4panel/xfce-panel-convenience.c
+++ libxfce4panel/xfce-panel-convenience.c
@@ -58,7 +58,7 @@ xfce_panel_create_button (void)
   GtkWidget *button = gtk_button_new ();
 
   gtk_widget_set_can_default (GTK_WIDGET (button), FALSE);
-  gtk_widget_set_can_focus (GTK_WIDGET (button), TRUE);
+  gtk_widget_set_can_focus (GTK_WIDGET (button), FALSE);
   gtk_button_set_relief (GTK_BUTTON (button), GTK_RELIEF_NONE);
   gtk_button_set_focus_on_click (GTK_BUTTON (button), FALSE);
   gtk_widget_set_name (button, "xfce-panel-button");
@@ -82,7 +82,7 @@ xfce_panel_create_toggle_button (void)
   GtkWidget *button = gtk_toggle_button_new ();
 
   gtk_widget_set_can_default (GTK_WIDGET (button), FALSE);
-  gtk_widget_set_can_focus (GTK_WIDGET (button), TRUE);
+  gtk_widget_set_can_focus (GTK_WIDGET (button), FALSE);
   gtk_button_set_relief (GTK_BUTTON (button), GTK_RELIEF_NONE);
   gtk_button_set_focus_on_click (GTK_BUTTON (button), FALSE);
   gtk_widget_set_name (button, "xfce-panel-toggle-button");
